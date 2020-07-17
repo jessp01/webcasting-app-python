eaa10ddaa580c7b3e819c5771ce615ae0a47033d
Jess portnoy <jess.portnoy@kaltura.com>
2020-07-17 18:21:37 +0100

    - Remove non-existing dep from requirements.txt     - Add config.template.py     - Set conversion profile in config.py rather than hard code it in create.py     - Higher version of Werkzeug (see https://github.com/pyeve/eve/issues/1331)

---
+++ config.template.py
@@ -0,0 +1,20 @@
+partner_id = 0000000
+admin_secret = "xxxxx"
+admin_email = "xxxxx"
+service_url = "https://www.kaltura.com/"
+conversionProfileId = 00000
+
+player_v2 = False
+uiconf_id = 00000000
+uiconf_idv2 = 00000000
+
+
+user_id = "xxxxx"
+moderator_user = "xxxxx"
+
+app_name = "Webcasting Application"
+app_id = "myCustomWebcastApp"
+app_domain = "my.customwebcast.com"
+
+
+
